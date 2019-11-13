c18961b8e5de27a5934afcd5713e158f37ec2d05
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-13 00:32:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,4 +5,14 @@ class Song
   @@genres = []
   @@artists = []
   
+   def initialize(song_name, artist, genre)
+    @name = song_name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+  
+  
 end
